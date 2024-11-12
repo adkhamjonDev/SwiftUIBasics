a1660e85d62d52741a2688dba4feb2516759463f
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-12 16:27:59 +0500

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Adkhamjon Rakhimov on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIBasicsTests/SwiftUIBasicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsTests.swift
+//  SwiftUIBasicsTests
+//
+//  Created by Adkhamjon Rakhimov on 12/11/24.
+//
+
+import Testing
+@testable import SwiftUIBasics
+
+struct SwiftUIBasicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
