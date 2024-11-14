cb6d4942d1515e714e6cb9ddce4b463318d867b3
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-14 12:41:13 +0500

    paddings

---
+++ SwiftUIBasics/SwiftUIBootcamp/Padding.swift
@@ -0,0 +1,18 @@
+//
+//  Padding.swift
+//  SwiftUIBasics
+//
+//  Created by Adkhamjon Rakhimov on 14/11/24.
+//
+
+import SwiftUI
+
+struct Padding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Padding()
+}
