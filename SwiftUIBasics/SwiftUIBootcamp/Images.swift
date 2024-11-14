869b2b48c4de94725d4bb08fdb50a36ac511f59b
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-14 10:28:56 +0500

    commit

---
+++ SwiftUIBasics/SwiftUIBootcamp/Images.swift
@@ -0,0 +1,18 @@
+//
+//  Images.swift
+//  SwiftUIBasics
+//
+//  Created by Adkhamjon Rakhimov on 14/11/24.
+//
+
+import SwiftUI
+
+struct Images: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Images()
+}
