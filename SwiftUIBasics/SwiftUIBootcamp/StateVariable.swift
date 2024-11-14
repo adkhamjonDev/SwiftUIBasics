b511c0bd3f1455e274df44035808f3be70998f65
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-14 18:08:00 +0500

    state

--- SwiftUIBasics/SwiftUIBootcamp/StateVariable.swift
+++ SwiftUIBasics/SwiftUIBootcamp/StateVariable.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct StateVariable: View {
+    
+    @State var backgroundColor:Color = .green
+    @State var myTitle:String = "My Title"
+    @State var count:Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            // background
+            
+            backgroundColor.ignoresSafeArea()
+            
+            // content
+            VStack(spacing:20){
+                Text("\(myTitle)")
+                    .font(.title)
+                Text("Count: \(count)")
+                    .font(.headline)
+                    .underline()
+                
+                
+                HStack(spacing: 20){
+                    Button("Minus"){
+                        self.backgroundColor = .red
+                        self.myTitle = "MINUS WAS PRESSED"
+                        self.count-=1
+                    }
+                    
+                    Button("Plus"){
+                        self.backgroundColor = .purple
+                        self.myTitle = "PLUS WAS PRESSED"
+                        self.count+=1
+
+                    }
+                }
+            }.foregroundColor(.white)
+        }
     }
 }
 
