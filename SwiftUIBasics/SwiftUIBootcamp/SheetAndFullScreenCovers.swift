4b2f6017361f7710a2d7d4ea7a4c821dc4f4d0ea
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-15 11:57:51 +0500

    sheets

--- SwiftUIBasics/SwiftUIBootcamp/SheetAndFullScreenCovers.swift
+++ SwiftUIBasics/SwiftUIBootcamp/SheetAndFullScreenCovers.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct SheetAndFullScreenCovers: View {
+    
+    @State var showSheet:Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.green.ignoresSafeArea()
+            
+            Button(
+                action:{
+                    showSheet.toggle()
+                }, label:{
+                    Text("Show Sheet")
+                        .foregroundColor(.green)
+                        .font(.headline)
+                        .padding(20)
+                        .background(Color.white.cornerRadius(10))
+                }
+            )
+//            .fullScreenCover(isPresented: $showSheet){
+//                SecondView()
+//            }
+            .sheet(isPresented: $showSheet){
+                SecondView()
+            }
+        }
+    }
+}
+
+struct SecondView:View{
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body:some View{
+        ZStack(alignment: .topLeading){
+            Color.red.ignoresSafeArea()
+            
+            Button(
+                action:{
+                    presentationMode.wrappedValue.dismiss()
+                }, label:{
+                    Image(systemName: "xmark")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .padding(20)
+                }
+            )
+        }
     }
 }
 
